bd1a8d6c1ab7634d756390b89ee7ab24e618216f
kris979 <kris979@localhost>
2012-03-17 15:03:45 +0000

    evolving example

--- src/main/java/bdd/TraderSteps.java
+++ src/main/java/bdd/TraderSteps.java
@@ -5,11 +5,12 @@
  */
 package bdd;
 
+import static org.junit.Assert.assertEquals;
+
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
-
-import static org.junit.Assert.*;
 
 /**
  * 
@@ -19,18 +20,18 @@ public class TraderSteps {
 
     private Stock stock;
     
-    @Given("a stock of symbol $symbol and a threshold of $threshold")
-    public void aStock(String symbol, double threshold) {
+    @Given("a stock of symbol <symbol> and a threshold of <threshold>")
+    public void aStock(@Named("symbol") String symbol, @Named("threshold") double threshold) {
         stock = new Stock(symbol, threshold);
     }
  
-    @When("the stock is traded at $price")
-    public void theStockIsTradedAt(double price) {
+    @When("the stock is traded at <price>")
+    public void theStockIsTradedAt(@Named("price") double price) {
         stock.tradeAt(price);
     }
  
-    @Then("the alert status should be $status")
-    public void theAlertStatusShouldBe(String status) {
+    @Then("the alert status should be <status>")
+    public void theAlertStatusShouldBe(@Named("status") String status) {
         assertEquals(status,stock.getStatus());
     }
 }
--- src/main/java/bdd/trader_test.story
+++ src/main/java/bdd/trader_test.story
@@ -1,11 +1,12 @@
-Scenario: trader is not alerted below threshold
- 
-Given a stock of symbol STK1 and a threshold of 10.0
-When the stock is traded at 5.0
-Then the alert status should be OFF
- 
-Scenario: trader is alerted above threshold
- 
-Given a stock of symbol STK1 and a threshold of 10.0
-When the stock is traded at 11.0
-Then the alert status should be ON
+Story: Trader
+
+Scenario: trader is not alerted below threshold 
+Given a stock of symbol <symbol> and a threshold of <threshold>
+When the stock is traded at <price>
+Then the alert status should be <status>
+
+Examples:
+|symbol|threshold|price|status
+|STK|10|5|OFF|
+|ATK|10|9|OFF|
+|STK|10|1|OFF|
