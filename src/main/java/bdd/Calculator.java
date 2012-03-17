830d7b1499058bfd8f1999c3ae0c73b27183915b
kris979 <kris979@localhost>
2012-03-17 14:09:20 +0000

    playing with JBehave

---
+++ src/main/java/bdd/Calculator.java
@@ -0,0 +1,24 @@
+/* 
+  * ============================================================================ 
+  * Name      : Calculator.java
+  * ============================================================================
+  */
+package bdd;
+
+/**
+ * 
+ *
+ */
+public class Calculator {
+
+    /**
+     * @param firstNumber
+     * @param secondNumber
+     * @return 
+     */
+    public int add(int firstNumber, int secondNumber) {
+        System.out.println("Adding " + firstNumber + " to " + secondNumber + ".");
+        return firstNumber + secondNumber;
+    }
+
+}
