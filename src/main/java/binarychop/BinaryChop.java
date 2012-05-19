619cccb7b335402e7723d861eb267d8534b5e5bc
kris979 <kris979@localhost>
2012-05-19 16:41:13 +0100

    binaryChop

--- src/main/java/binarychop/BinaryChop.java
+++ src/main/java/binarychop/BinaryChop.java
@@ -16,10 +16,10 @@ public class BinaryChop {
         if (arrayToSearch.length == 0) {
             return -1;
         }
-        return check(digitToFind, 0, arrayToSearch.length - 1, arrayToSearch);
+        return recursive(digitToFind, 0, arrayToSearch.length - 1, arrayToSearch);
     }
 
-    private int check(int digitToFind, int beginIndex, int endIndex, int[] arrayToSearch) {
+    private int recursive(int digitToFind, int beginIndex, int endIndex, int[] arrayToSearch) {
         if (endIndex - beginIndex == 0) {
             if (arrayToSearch[beginIndex] == digitToFind) {
                 return beginIndex;
@@ -31,10 +31,45 @@ public class BinaryChop {
         if (digitToFind == arrayToSearch[middleIndex]) {
             return middleIndex;
         } else if (digitToFind <= arrayToSearch[middleIndex]) {
-            return check(digitToFind, beginIndex, middleIndex, arrayToSearch);
+            return recursive(digitToFind, beginIndex, middleIndex, arrayToSearch);
         } else {
-            return check(digitToFind, middleIndex+1, endIndex, arrayToSearch);
+            return recursive(digitToFind, middleIndex+1, endIndex, arrayToSearch);
         }
     }
 
+    
+    
+    
+    public int iterativeChop(int digitToFind, int[] arrayToSearch) {
+        int position = -1;
+        if (arrayToSearch.length == 0) {
+            return -1;
+        }
+        int beginIndex = 0;
+        int endIndex = arrayToSearch.length - 1;
+        
+        while (endIndex - beginIndex > 0) {
+            int middleIndex = beginIndex + (endIndex - beginIndex) / 2;
+            if (digitToFind == arrayToSearch[middleIndex]) {
+                return middleIndex;
+            } else if (digitToFind <= arrayToSearch[middleIndex]) {
+                    endIndex = middleIndex;
+            } else {
+                    beginIndex = middleIndex+1;
+            }
+        } 
+        if (endIndex - beginIndex == 0) {
+            if (arrayToSearch[beginIndex] == digitToFind) {
+                return beginIndex;
+            } else {
+                return -1;
+            }
+        }
+        return position;
+    }
+    
+    private int iterative(int digitToFind, int beginIndex, int endIndex, int[] arrayToSearch) {
+        return endIndex;
+        
+    }
 }
--- src/test/java/binarychop/BinaryChopTest.java
+++ src/test/java/binarychop/BinaryChopTest.java
@@ -20,7 +20,7 @@ public class BinaryChopTest {
     private BinaryChop binaryChop = new BinaryChop();
     
     @Test
-    public void binarybinaryChopTest() {
+    public void recursiveChopTest() {
         assertEquals(-1, binaryChop.chop(3, new int[]{}));
         assertEquals(-1, binaryChop.chop(3, new int[]{1}));
         assertEquals(0,  binaryChop.chop(1, new int[]{1}));
@@ -40,6 +40,30 @@ public class BinaryChopTest {
         assertEquals(-1, binaryChop.chop(4, new int[]{1, 3, 5, 7}));
         assertEquals(-1, binaryChop.chop(6, new int[]{1, 3, 5, 7}));
         assertEquals(-1, binaryChop.chop(8, new int[]{1, 3, 5, 7}));
+        assertEquals(7, binaryChop.chop(9, new int[]{1, 2, 3, 4, 5, 7, 8, 9}));
     }
 
+    @Test
+    public void iterativeChopTest() {
+        assertEquals(-1, binaryChop.iterativeChop(3, new int[]{}));
+        assertEquals(-1, binaryChop.iterativeChop(3, new int[]{1}));
+        assertEquals(0,  binaryChop.iterativeChop(1, new int[]{1}));
+        assertEquals(0,  binaryChop.iterativeChop(1, new int[]{1, 3, 5}));
+        assertEquals(1,  binaryChop.iterativeChop(3, new int[]{1, 3, 5}));
+        assertEquals(2,  binaryChop.iterativeChop(5, new int[]{1, 3, 5}));
+        assertEquals(-1, binaryChop.iterativeChop(0, new int[]{1, 3, 5}));
+        assertEquals(-1, binaryChop.iterativeChop(2, new int[]{1, 3, 5}));
+        assertEquals(-1, binaryChop.iterativeChop(4, new int[]{1, 3, 5}));
+        assertEquals(-1, binaryChop.iterativeChop(6, new int[]{1, 3, 5}));
+        assertEquals(0,  binaryChop.iterativeChop(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1,  binaryChop.iterativeChop(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2,  binaryChop.iterativeChop(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3,  binaryChop.iterativeChop(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.iterativeChop(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.iterativeChop(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.iterativeChop(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.iterativeChop(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.iterativeChop(8, new int[]{1, 3, 5, 7}));
+        assertEquals(7, binaryChop.iterativeChop(9, new int[]{1, 2, 3, 4, 5, 7, 8, 9}));
+    }
 }
