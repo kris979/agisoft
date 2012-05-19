38c2f25a575a47a6250888684806657e39482861
kris979 <kris979@localhost>
2012-05-19 15:57:50 +0100

    recursivebinaryChop

--- src/main/java/binarychop/BinaryChop.java
+++ src/main/java/binarychop/BinaryChop.java
@@ -1,38 +1,39 @@
 /* 
-  * ============================================================================ 
-  * Name      : BinaryChop.java
-  * ============================================================================
-  */
+ * ============================================================================ 
+ * Name      : BinaryChop.java
+ * ============================================================================
+ */
 package binarychop;
 
-import java.util.Arrays;
 
 /**
  * 
  *
  */
 public class BinaryChop {
-    
+
     public int chop(int digitToFind, int[] arrayToSearch) {
         if (arrayToSearch.length == 0) {
             return -1;
         }
-        if (arrayToSearch.length == 1) {
-            if (arrayToSearch[0] == digitToFind) {
-                return 0;
+        return check(digitToFind, 0, arrayToSearch.length - 1, arrayToSearch);
+    }
+
+    private int check(int digitToFind, int beginIndex, int endIndex, int[] arrayToSearch) {
+        if (endIndex - beginIndex == 0) {
+            if (arrayToSearch[beginIndex] == digitToFind) {
+                return beginIndex;
             } else {
                 return -1;
             }
         }
-        
-        int middleIndex = arrayToSearch.length/2;
-        
+        int middleIndex = beginIndex + (endIndex - beginIndex) / 2;
         if (digitToFind == arrayToSearch[middleIndex]) {
             return middleIndex;
-        } else if (digitToFind < arrayToSearch[middleIndex]) {
-            return chop(digitToFind, Arrays.copyOfRange(arrayToSearch, 0, middleIndex));
+        } else if (digitToFind <= arrayToSearch[middleIndex]) {
+            return check(digitToFind, beginIndex, middleIndex, arrayToSearch);
         } else {
-            return middleIndex + chop(digitToFind, Arrays.copyOfRange(arrayToSearch, middleIndex, arrayToSearch.length));
+            return check(digitToFind, middleIndex+1, endIndex, arrayToSearch);
         }
     }
 
--- src/test/java/binarychop/BinaryChopTest.java
+++ src/test/java/binarychop/BinaryChopTest.java
@@ -30,40 +30,16 @@ public class BinaryChopTest {
         assertEquals(-1, binaryChop.chop(0, new int[]{1, 3, 5}));
         assertEquals(-1, binaryChop.chop(2, new int[]{1, 3, 5}));
         assertEquals(-1, binaryChop.chop(4, new int[]{1, 3, 5}));
-//        assertEquals(-1, binaryChop.chop(6, new int[]{1, 3, 5}));
-//        assertEquals(0,  binaryChop.chop(1, new int[]{1, 3, 5, 7}));
-//        assertEquals(1,  binaryChop.chop(3, new int[]{1, 3, 5, 7}));
-//        assertEquals(2,  binaryChop.chop(5, new int[]{1, 3, 5, 7}));
-//        assertEquals(3,  binaryChop.chop(7, new int[]{1, 3, 5, 7}));
-//        assertEquals(-1, binaryChop.chop(0, new int[]{1, 3, 5, 7}));
-//        assertEquals(-1, binaryChop.chop(2, new int[]{1, 3, 5, 7}));
-//        assertEquals(-1, binaryChop.chop(4, new int[]{1, 3, 5, 7}));
-//        assertEquals(-1, binaryChop.chop(6, new int[]{1, 3, 5, 7}));
-//        assertEquals(-1, binaryChop.chop(8, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(6, new int[]{1, 3, 5}));
+        assertEquals(0,  binaryChop.chop(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1,  binaryChop.chop(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2,  binaryChop.chop(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3,  binaryChop.chop(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, binaryChop.chop(8, new int[]{1, 3, 5, 7}));
     }
 
 }
-
-
-
-//def where_is_it(val, left, right, container):
-//    l = right - left
-//    if(l == 0):
-//    if(container[left] == val):
-//    return left
-//    else:
-//    return -1
-//
-//    if(container[left + l/2] >= val):
-//    return where_is_it(val, left, left + l/2, container)
-//    else:
-//    return where_is_it(val, left + l/2+1 , right , container)
-//
-//
-//    if __name__ == "__main__":
-//
-//    ints = [1,2,3,4,5]
-//
-//    global count    
-//    for i in range(1,10):
-//    print "%d is at? %d\n\n" % (i, where_is_it(i, 0, len(ints)-1, ints))
