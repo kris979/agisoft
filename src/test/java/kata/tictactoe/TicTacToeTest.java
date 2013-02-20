17bceb8f83a82d1d3acac3b172a3a17c6ccd18ec
kris979 <kris979@gmail.com>
2013-02-20 22:20:43 +0000

    a

--- src/test/java/kata/tictactoe/TicTacToeTest.java
+++ src/test/java/kata/tictactoe/TicTacToeTest.java
@@ -15,9 +15,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * a game is over when all fields are taken a game is over when all fields in a column are taken by a player a game is
- * over when all fields in a row are taken by a player a game is over when all fields in a diagonal are taken by a
- * player a player can take a field if not already taken players take turns taking fields until the game is over
+ * a game is over when all fields are taken 
+ * a game is over when all fields in a column are taken by a player 
+ * a game is over when all fields in a row are taken by a player 
+ * a game is over when all fields in a diagonal are taken by a player 
+ * a player can take a field if not already taken 
+ * players take turns taking fields until the game is over
  */
 public class TicTacToeTest {
 
